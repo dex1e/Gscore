175e4ade94cb2f2aa36d5dd6581dd0d15aa5b882
dex1e <dex1e5@yandex.ru>
2022-08-23 19:52:47 +0600

    fix: adaptive start-subscription

--- src/Layout/index.ts
+++ src/Layout/index.ts
@@ -1,2 +1,1 @@
-export { ContentLayout } from "./ContentLayout";
 export { MainLayout } from "./MainLayout";
--- src/pages/start-subscription.tsx
+++ src/pages/start-subscription.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import styled from "styled-components";
 
-import { DEVICE } from "assets";
+import { COLORS, DEVICE } from "assets";
 import { PackageCard } from "components";
 import { LinkButton } from "components/ui";
 import { ROUTES } from "constant";
-import { ContentLayout, MainLayout } from "Layout";
+import { MainLayout } from "Layout";
 import { useAppSelector } from "store/hooks";
 
 const StartSubscriptionPage = () => {
@@ -45,18 +45,27 @@ const Root = styled(MainLayout)`
   width: 100%;
 `;
 
-const Container = styled(ContentLayout)`
+const Container = styled.div`
+  color: ${COLORS.neutral100};
+  overflow: hidden;
+  max-width: 620px;
+  margin: 0 auto;
+  padding: 42px 0 90px;
+
   @media ${DEVICE.laptop} {
-    margin-bottom: 390px;
+    padding: 72px 0 390px;
   }
 `;
 
 const Title = styled.h1`
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 30px;
+  padding-bottom: 16px;
+
   @media ${DEVICE.laptop} {
-    font-weight: 700;
     font-size: 44px;
     line-height: 54px;
-    padding-bottom: 16px;
   }
 `;
 
@@ -64,20 +73,24 @@ const Text = styled.p`
   font-weight: 500;
   font-size: 14px;
   line-height: 24px;
-  padding-bottom: 48px;
+  padding-bottom: 24px;
+
+  @media ${DEVICE.laptop} {
+    padding-bottom: 48px;
+  }
 `;
 
 const StyledPackageCard = styled(PackageCard)`
+  margin-bottom: 24px;
+
   @media ${DEVICE.laptop} {
     margin-bottom: 48px;
   }
 `;
 
 const StyledLinkButton = styled(LinkButton)`
-  @media ${DEVICE.laptop} {
-    max-width: 100%;
-    width: 100%;
-  }
+  max-width: 100%;
+  width: 100%;
 `;
 
 export default StartSubscriptionPage;
